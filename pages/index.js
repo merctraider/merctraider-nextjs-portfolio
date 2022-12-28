8bcc6640b6971eb288be7455850a4531b0c31ccf
merctraider <47917658+merctraider@users.noreply.github.com>
2022-12-28 20:43:50 +0800

    seo

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,11 @@ export default function Home() {
     <div>
       <Head>
         <title>merctraider | Jonathan Teo's Portfolio</title>
+        <meta name='description' content='I am Jonathan Teo, A Web and Game Developer.  Right now, I love doing web design and development. While my background is programming in Game Design and Development, I have recently branched out into full-stack web development.'/>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="/assets/thumb.png"></meta>
+        <meta property="og:type" content="website" />
+
       </Head>
 
       <div className='bg-prism-pattern absolute w-full h-full opacity-20 z-[-10] backdrop-blur-sm'></div>
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,7 +6,11 @@ export default function ProjectsPage() {
     <div>
       <Head>
         <title>merctraider | Jonathan Teo's Projects</title>
+        <meta name='description' content='I am Jonathan Teo, and these are my projects. Most of them are web related'/>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="/assets/thumb.png"/>
+        <meta property="og:type" content="website" />
+
       </Head>
 
       <Navbar/>
